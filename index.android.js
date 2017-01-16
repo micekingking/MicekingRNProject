4e12fe72851a1f8b0e8e6d12e47b208a0db5b1ee
wanghao01 <wanghao01@vcredit.com>
2017-01-16 18:15:33 +0800

    从网络加载数据,展示

--- index.android.js
+++ index.android.js
@@ -21,22 +21,88 @@ var MOCKED_MOVIES_DATA = [
   {title: '标题', year:'2015', posters:{thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 export default class MicekingProject extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+        movies: null,//初始化state变量值
+    };
 
-    //let pic = {uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'};
-    var movie = MOCKED_MOVIES_DATA[0];
-    return (
+    // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向不对
+    // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+          //注意这里使用this, 为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+          this.setState({
+            movies: responseData.movies,
+          });
+      })
+      .catch(function(error) {
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+        // ADD THIS THROW error
+        throw error;
+      });
+
+  }
+
+renderLoadingView(movie) {
+  return (
       <View style={styles.container}>
-        <Image source={{uri: movie.posters.thumbnail}}
-         style={styles.thumbnail}/>
-
-      <View style={styles.rightContainer}>
-        <Text style={styles.title}>{movie.title}</Text>
-        <Text style={styles.year}>{movie.year}</Text>
-      </View>
+        <Text>
+          正在加载电影数据……
+        </Text>
       </View>
     );
+}
+
+renderMovie(movie) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{uri: movie.posters.thumbnail}}
+        style={styles.thumbnail}/>
+    
+    <View style={styles.rightContainer}>
+        <Text style={styles.title}>{movie.title}</Text>
+        <Text style={styles.year}>{movie.year}</Text>
+    </View>
+
+    </View>
+  );
+}
+
+  render() {
+    if(!this.state.movies) {
+      return this.renderLoadingView();
+    } 
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+
+    //let pic = {uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'};
+    // var movie = MOCKED_MOVIES_DATA[0];
+    // return (
+    //   <View style={styles.container}>
+    //     <Image source={{uri: movie.posters.thumbnail}}
+    //      style={styles.thumbnail}/>
+
+    //   <View style={styles.rightContainer}>
+    //     <Text style={styles.title}>{movie.title}</Text>
+    //     <Text style={styles.year}>{movie.year}</Text>
+    //   </View>
+    //   </View>
+    // );
   }
 }
 
